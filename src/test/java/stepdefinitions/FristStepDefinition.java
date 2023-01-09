d05078393a1579648a7501c40c02dc0918d5080d
QA Murat Tas <itmmt38@gmail.com>
2023-01-09 21:55:28 +0300

    cucumber

---
+++ src/test/java/stepdefinitions/FristStepDefinition.java
@@ -0,0 +1,28 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FristStepDefinition {
+
+    @Given("Ilk feature icin cucumber indirdim")
+    public void ılk_feature_icin_cucumber_indirdim() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Senaryomu calistirdigimda")
+    public void senaryomu_calistirdigimda() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Senaryomun calistigini konsolda gormeliyim")
+    public void senaryomun_calistigini_konsolda_gormeliyim() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+}
