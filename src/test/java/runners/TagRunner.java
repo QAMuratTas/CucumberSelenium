c5868690dd1cb6d0eeae432bf3c17c962fe44646
QA Murat Tas <itmmt38@gmail.com>
2023-01-09 23:05:53 +0300

    cucumber

--- src/test/java/runners/TagRunner.java
+++ src/test/java/runners/TagRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features ="src/test/resources/features/",
         glue = "stepdefinitions",
-        tags = "@smoke",
+        tags = "@smoke or @regression", // diğer tagleri de çalıştırmak için araya or koyarak tagleri yazıyoruz.
         dryRun = false
 
 )
