ff2745216d51ea13f1b635384bb790757917ecd0
QA Murat Tas <itmmt38@gmail.com>
2023-01-09 22:58:57 +0300

    cucumber

--- src/test/java/runners/TagRunner.java
+++ src/test/java/runners/TagRunner.java
@@ -1,4 +1,19 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+//sadece smoke tagine sahip olan senaryoları çalıştıran runner
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features ="src/test/resources/features/",
+        glue = "stepdefinitions",
+        tags = "@smoke",
+        dryRun = false
+
+)
+
+
 public class TagRunner {
 }
---
+++ target/test-classes/features/01_Tags.feature
@@ -0,0 +1,13 @@
+Feature: Taglerin Kullanimi
+
+  @smoke
+  Scenario: Smoke Test Senaryosu
+    Given Smoke Test icin hazirliklar yapildi
+    When Smoke Test Calistiginda
+    Then Smoke Test basarili olmali
+
+  @regression
+  Scenario: Regression Test Senaryosu
+    Given Regression Test icin hazirliklar yapildi
+    When Regression Test Calistiginda
+    Then Regression Test basarili olmali
